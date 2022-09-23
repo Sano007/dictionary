ca0b663654efa7d921b7e4073663b10331137434
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-09-23 12:58:05 +0200

    writte_node.c: made add_node()

--- writte_node.c
+++ writte_node.c
@@ -4,8 +4,9 @@
 #include "writte_node.h"
 
 
-struct Dict* new_node(char* word){
-	struct Dict* new = calloc(1, sizeof(struct Dict));
+struct Dict *new_node(char *word)
+{
+	struct Dict *new = calloc(1, sizeof(struct Dict));
 	new->children = NULL;
 	new->nb_children = 0;
 	new->is_word = true;
@@ -15,24 +16,45 @@ struct Dict* new_node(char* word){
 }
 
 
-struct Dict* add_node(struct Dict* self, char* word){
-	if(self == NULL) return NULL;
-	if(word == NULL) return NULL;
-	if(self->nb_children > 0){
-		bool is_new = true;
-		for(int i = 0; i < self->nb_children; i++){
-			if(self->children[i]->name == word[0]) {
+struct Dict *add_node(struct Dict *self, char *word)
+{
+	if (self == NULL)
+		return NULL;
+	if (word == NULL || word[0] == '\0')
+		return NULL;
+	if (self->nb_children > 0)
+	{
+		for (int i = 0; i < self->nb_children; i++)
+		{
+			if (self->children[i]->name == word[0])
+			{
 				self->children[i] = add_node(self->children[i], &word[1]);
-				is_new = false;
-				break;
+				return self;
 			}
 		}
-		if(is_new){
+		self->nb_children++;
+		struct Dict **new_children = calloc(self->nb_children, sizeof(struct Dict *));
+		memcpy(new_children, self->children, self->nb_children);
+		free(self->children);
+		new_children[self->nb_children - 1] = new_node(&word[1]);
+	}
+	else
+	{
+		if (self->last_word == NULL)
+		{
 			self->nb_children++;
-			struct Dict** new_children = calloc(self->nb_children, sizeof(struct Dict*));
-			memcpy(new_children, self->children, self->nb_children);
-			free(self->children);
-			new_children[self->nb_children-1] = new_node(&word[1]);
+			self->children = calloc(1, sizeof(struct Dict *));
+			self->children[0] = new_node(&word[1]);
+		}
+		else
+		{
+			self->nb_children = 2;
+			self->children = calloc(2, sizeof(struct Dict *));
+			self->children[1] = new_node(&word[1]);
+			self->children[0] = new_node(self->last_word);
+			self->is_word = false;
+			self->last_word = NULL;
 		}
 	}
+	return self;
 }
